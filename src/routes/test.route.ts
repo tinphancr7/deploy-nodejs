90c6518beb4994d2b7652c143a2be855f8b30251
Tin Phan <phanthanhtincr7@gmail.com>
2025-05-02 17:40:12 +0700

    up

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -2,7 +2,7 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
 export default async function testRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
   fastify.get('/', async (request, reply) => {
-    reply.sends({
+    reply.send({
       message: 'Hello from test route 1!'
     })
   })
